9ed3ad3d3887363bc7d9cc7fb24c286dcaaf7e2c
gautammmerina3 <gautammmerina3@gmail.com>
2019-11-12 15:05:58 +0545

    show added

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('questions', 'QuestionsController');
+Route::resource('questions', 'QuestionsController')->except('show');
 
-Auth::routes();
+Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
 
-Route::get('/home', 'HomeController@index')->name('home');
+
