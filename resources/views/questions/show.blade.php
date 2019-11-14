823a8f9a3639d7db51be5294b73c02c53d55d6be
gautammmerina3 <gautammmerina3@gmail.com>
2019-11-14 15:45:19 +0545

    favorite questions

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -27,13 +27,27 @@
                                 <a title="This question is not useful" class="vote-down off">
                                     <i class="far fa-thumbs-down fa-2x"></i> 
                                 </a>
-                                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                
+                                <a title="Click to mark as favorite question (Click again to undo)" 
+                                    class="favorite mt-2
+                                    {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
+                                    onClick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                                    >
                                     <i class="fas fa-star fa-2x"></i>
-                                    <span class="favorites-count">123</span>
+                                    <span class="favorites-count">{{ $question->favorites_count  }}</span>
                                 </a>
+
+                                <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="post" style="display:none;">
+                                    @csrf
+                                    @if($question->is_favorited)
+                                        @method('DELETE')
+                                    @endif
+                                </form>
+
+
                             </div>
                             <div class="media-body">
-                                {!! $question->body !!}
+                                {!! $question->body !!} 
                                 <div class="float-right mt-4">
                                     <span class="text-muted"> Questioned  {{ $question->created_date }}</span>
                                     <div class="media mt-2">
