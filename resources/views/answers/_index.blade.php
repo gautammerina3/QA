12977fbf03dd20932fc1710e35c144c193a893de
gautammmerina3 <gautammmerina3@gmail.com>
2019-11-19 12:04:48 +0545

    implement DRY principle on vote

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -31,24 +31,8 @@
                                     @csrf
                                     <input type="hidden" name="vote" value="-1">
                                 </form>
-                            @can('accept', $answer)
-                                <a title="Mark this answer as best answer " 
-                                    class="{{ $answer->status }} mt-2"
-                                    onClick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                                    <i class="fas fa-check fa-lg"></i>
-                                </a>
-                            
-                                <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="post" style="display:none;">
-                                    @csrf
-                                </form>
-                            @else
-                                @if($answer->is_best)
-                                    <a title="The question owner accepted this answer as best answer " 
-                                        class="{{ $answer->status }} mt-2">
-                                        <i class="fas fa-check fa-lg"></i>
-                                    </a>
-                                @endif
-                            @endcan
+                                <!-- @include('shared._favorite')
+                                @include('shared._vote') -->
 
                         </div>
                         <div class="media-body">
@@ -74,17 +58,10 @@
 
                                 </div>
                                 <div class="col-4">
-                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}" width="40" height="40">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">
-                                                {{ $answer->user->name }}
-                                            </a>
-                                        </div>
-                                    </div>
+                                    @include('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answered'    
+                                    ])
                                 </div>
                             </div>
                         </div>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -19,60 +19,19 @@
                         <hr>
  
                         <div class="media"> 
-                            <div class="d-flex flex-column vote-controls">
-                                <a title="This question is useful" 
-                                    class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                                    onClick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit();"
-                                    >
-                                    <i class="far fa-thumbs-up fa-2x"></i>
-                                </a>
-                                <form id="up-vote-question-{{ $question->id }}" action="/questions/{{ $question->id }}/vote" method="post" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
-                                <span class="votes-count">{{$question->votes_count}}</span>
-                                <a title="This question is not useful" 
-                                    class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                                    onClick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit();"
-                                    >
-                                    <i class="far fa-thumbs-down fa-2x"></i> 
-                                </a>
-                                <form id="down-vote-question-{{ $question->id }}" action="/questions/{{ $question->id }}/vote" method="post" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
-                                
-                                <a title="Click to mark as favorite question (Click again to undo)" 
-                                    class="favorite mt-2
-                                    {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                                    onClick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
-                                    >
-                                    <i class="fas fa-star fa-2x"></i>
-                                    <span class="favorites-count">{{ $question->favorites_count  }}</span>
-                                </a>
-
-                                <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="post" style="display:none;">
-                                    @csrf
-                                    @if($question->is_favorited)
-                                        @method('DELETE')
-                                    @endif
-                                </form>
-
-
-                            </div>
+                            @include('shared._vote', [
+                                'model' => $question
+                            ])
                             <div class="media-body">
                                 {!! $question->body !!} 
-                                <div class="float-right mt-4">
-                                    <span class="text-muted"> Questioned  {{ $question->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $question->user->url }}" class="pr-2">
-                                            <img src="{{ $question->user->avatar }}" width="40" height="40">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $question->user->url }}">
-                                                {{ $question->user->name }}
-                                            </a>
-                                        </div>
+                                <div class="row">
+                                    <div class="col-4"></div>
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include('shared._author', [
+                                            'model' => $question,
+                                            'label' => 'Asked'
+                                        ])
                                     </div>
                                 </div>
                             </div>
