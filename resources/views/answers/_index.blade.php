7aa1a004c24c416b62ecf9e3bef44596fb260b7b
gautammmerina3 <gautammmerina3@gmail.com>
2019-11-19 14:00:12 +0545

    tidy up the views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,66 +9,8 @@
                 </div>
                 <hr>
                 @include('layouts._messages')
-                @foreach($answers as $answer)
-                    <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                        <a title="This answer is useful" 
-                                    class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                                    onClick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();"
-                                    >
-                                    <i class="far fa-thumbs-up fa-2x"></i>
-                                </a>
-                                <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="post" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
-                                <span class="votes-count">{{$answer->votes_count}}</span>
-                                <a title="This question is not useful" 
-                                    class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                                    onClick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();"
-                                    >
-                                    <i class="far fa-thumbs-down fa-2x"></i> 
-                                </a>
-                                <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="post" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
-                                <!-- @include('shared._favorite')
-                                @include('shared._vote') -->
-
-                        </div>
-                        <div class="media-body">
-                            {!! $answer->body !!}
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="ml-auto">
-                                        @can('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id] )}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-
-                                        @can('delete', $answer)
-                                            <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id])}}" method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onClick="return confirm('Are you sure?')">Delete</button>
-                                            </form>
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="col-4">
-
-
-                                </div>
-                                <div class="col-4">
-                                    @include('shared._author', [
-                                        'model' => $answer,
-                                        'label' => 'answered'    
-                                    ])
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
+                @foreach($answers as $answer) 
+                    @include('answers._answer') 
                 @endforeach
             </div>
         </div>
